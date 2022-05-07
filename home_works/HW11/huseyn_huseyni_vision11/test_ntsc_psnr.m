24c1593a736441151f64411140602ba50a03ef4b
Huseyn Huseyni <72157364+HuseynHuseyni@users.noreply.github.com>
2022-05-06 20:55:10 -0600

    Some change in HW11

--- home_works/HW11/huseyn_huseyni_vision11/test_ntsc_psnr.m
+++ home_works/HW11/huseyn_huseyni_vision11/test_ntsc_psnr.m
@@ -1,10 +1,20 @@
 function test_ntsc_psnr()
-x = imread('autumn.tif');
-imshow(x);
-myRandomNTSC = [0.3 0.5 0.1;0.6 -0.3 -0.3;0.2 -0.6 0.312];
-myRGB = cat(3,double(x(:, :, 1)),double(x(:, :, 2)),double(x(:, :, 3)));
-myYIQ = myRandomNTSC * myRGB;
-figure;
-imshow(myYIQ);
-end
+    x = imread('autumn.tif');
+    imshow(x);
 
+    myYIQ =[   0.3.* double(x(:, :, 1)) +  0.5.* double(x(:, :, 2)) +  0.1 .* double(x(:, :, 3));
+               0.6.* double(x(:, :, 1)) - -0.3.* double(x(:, :, 2)) - -0.3 .* double(x(:, :, 3));
+               0.2.* double(x(:, :, 1)) - -0.6.* double(x(:, :, 2)) +  0.3 .* double(x(:, :, 3))];
+    figure;
+    imshow(myYIQ);
+
+    I = imread("autumn.tif");
+    imshow(I);
+    
+    ntscimage = rgb2ntsc(I);
+    figure;
+    imshow(ntscimage);
+
+    psnr(ntscimage,myYIQ);
+
+end
