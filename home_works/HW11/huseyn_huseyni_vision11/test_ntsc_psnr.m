10d0962f228a880f9edb65be8677bec2ba31f7a0
Huseyn Huseyni <72157364+HuseynHuseyni@users.noreply.github.com>
2022-05-08 00:54:34 -0600

    Fix dimension error in myYIQ

--- home_works/HW11/huseyn_huseyni_vision11/test_ntsc_psnr.m
+++ home_works/HW11/huseyn_huseyni_vision11/test_ntsc_psnr.m
@@ -1,20 +1,27 @@
 function test_ntsc_psnr()
     x = imread('autumn.tif');
+    subplot(2, 2, 1);
     imshow(x);
 
-    myYIQ =[   0.3.* double(x(:, :, 1)) +  0.5.* double(x(:, :, 2)) +  0.1 .* double(x(:, :, 3));
-               0.6.* double(x(:, :, 1)) - -0.3.* double(x(:, :, 2)) - -0.3 .* double(x(:, :, 3));
-               0.2.* double(x(:, :, 1)) - -0.6.* double(x(:, :, 2)) +  0.3 .* double(x(:, :, 3))];
-    figure;
+    myYIQ_R = (0.3 .* double(x(:, :, 1)) + 0.5 .* double(x(:, :, 2)) + 0.1 .* double(x(:, :, 3)));
+    myYIQ_G = (0.6 .* double(x(:, :, 1)) - -0.3 .* double(x(:, :, 2)) - -0.3 .* double(x(:, :, 3)));
+    myYIQ_B = (0.2 .* double(x(:, :, 1)) - -0.6 .* double(x(:, :, 2)) + 0.3 .* double(x(:, :, 3)));
+
+    myYIQ = cat(3, myYIQ_R, myYIQ_G, myYIQ_B);
+
+    subplot(2, 2, 3);
     imshow(myYIQ);
 
     I = imread("autumn.tif");
+    subplot(2, 2, 2);
     imshow(I);
-    
+
     ntscimage = rgb2ntsc(I);
-    figure;
+    subplot(2, 2, 4);
     imshow(ntscimage);
 
-    psnr(ntscimage,myYIQ);
+    compare_result = psnr(ntscimage, myYIQ);
+    output = ["Compare result with PSNR : ", compare_result];
+    disp(output);
 
 end
