b4fc40e93af77f249bda23abb4bf37829708f43c
Huseyn Huseyni <72157364+HuseynHuseyni@users.noreply.github.com>
2022-05-08 05:42:25 -0600

    Do HW11 at 1401-02-18

--- home_works/HW11/huseyn_huseyni_vision11/test_ntsc_psnr.m
+++ home_works/HW11/huseyn_huseyni_vision11/test_ntsc_psnr.m
@@ -1,18 +1,18 @@
 function test_ntsc_psnr()
-    x = imread('autumn.tif');
+    x = imread('baby.jpg');
     subplot(2, 2, 1);
     imshow(x);
 
-    myYIQ_R = (0.3 .* double(x(:, :, 1)) + 0.5 .* double(x(:, :, 2)) + 0.1 .* double(x(:, :, 3)));
-    myYIQ_G = (0.6 .* double(x(:, :, 1)) - -0.3 .* double(x(:, :, 2)) - -0.3 .* double(x(:, :, 3)));
-    myYIQ_B = (0.2 .* double(x(:, :, 1)) - -0.6 .* double(x(:, :, 2)) + 0.3 .* double(x(:, :, 3)));
+    myYIQ_R = (0.299 .* double(x(:, :, 1)) + 0.578 .* double(x(:, :, 2)) + 0.114 .* double(x(:, :, 3)));
+    myYIQ_G = (0.596 .* double(x(:, :, 1)) - -0.274 .* double(x(:, :, 2)) - -0.322 .* double(x(:, :, 3)));
+    myYIQ_B = (0.211 .* double(x(:, :, 1)) - -0.523 .* double(x(:, :, 2)) + 0.312 .* double(x(:, :, 3)));
 
-    myYIQ = cat(3, myYIQ_R, myYIQ_G, myYIQ_B);
+    myYIQ = uint8(cat(3, myYIQ_R, myYIQ_G, myYIQ_B));
 
     subplot(2, 2, 3);
     imshow(myYIQ);
 
-    I = imread("autumn.tif");
+    I = imread("baby.jpg");
     subplot(2, 2, 2);
     imshow(I);
 
@@ -20,7 +20,7 @@ function test_ntsc_psnr()
     subplot(2, 2, 4);
     imshow(ntscimage);
 
-    compare_result = psnr(ntscimage, myYIQ);
+    compare_result = psnr(double(ntscimage), double(myYIQ));
     output = ["Compare result with PSNR : ", compare_result];
     disp(output);
 
